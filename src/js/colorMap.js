0fd894b2b5d37e1510f42a2b6e2367e3cd086205
Timothy Earley <timmy@earley.de>
2020-12-03 21:43:36 +0100

    Updated Team Names

--- src/js/colorMap.js
+++ src/js/colorMap.js
@@ -2,7 +2,7 @@
 
 const colorMap = new Map();
 
-colorMap.set("FC Bayern", ['#df2127', '#df2127']);
+colorMap.set("FC Bayern München", ['#df2127', '#df2127']);
 colorMap.set("TSG 1899 Hoffenheim", ['#1c63b7', '#1c63b7']);
 colorMap.set("Hertha BSC", ['#005eaa', '#ffffff']);
 colorMap.set("Werder Bremen", ['#009556', '#009556']);
@@ -16,27 +16,16 @@ colorMap.set("Borussia Mönchengladbach", ['#000000', '#000000']);
 colorMap.set("Bayer Leverkusen", ['#e4210b', '#e4210b']);
 colorMap.set("1. FSV Mainz 05", ['#e62100', '#e62100']);
 colorMap.set("VfB Stuttgart", ['#f22b1a', '#ffffff']);
-colorMap.set("Borussia Dortmund", ['#ffe800', '#ffe800']);
+colorMap.set("BV Borussia Dortmund 09", ['#ffe800', '#ffe800']);
 colorMap.set("RB Leipzig", ['#e0223c', '#e0223c']);
 colorMap.set("Fortuna Düsseldorf", ['#ffffff', '#ffffff']);
 colorMap.set("1. FC Nürnberg", ['#ad1732', '#ad1732']);
 colorMap.set("SC Paderborn 07", ['#005ea8', '#005ea8']);
 colorMap.set("1. FC Union Berlin", ['#d20303', '#feec83']);
 colorMap.set("1. FC Köln", ['#ffffff', '#eb2206']);
+colorMap.set("Arminia Bielefeld", ['#015092', '#000000'])
 //colorMap.set("Hamburger SV", '#004087 ');
 
-
-// Broken: Köln, Düsseldorf, Gladbach -> Umlaute?
-
-const extraColors = [
-	['#ff00ff', '#ff00ff']
-];
-var i = 0;
-function nextColor() {
-	const c = extraColors[i];
-	i = (i + 1) % extraColors.length;
-	return c;
-}
 
 function getColor(team) {
 	if (colorMap.has(team)) {
